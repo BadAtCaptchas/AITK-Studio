2360197e307b83f09621aaf6540a41d636302709
Ray <ray@raymccann.com>
2026-05-23 17:55:36 -0400

    feat: port selected z-image upstream changes

--- extensions_built_in/diffusion_models/z_image/__init__.py
+++ extensions_built_in/diffusion_models/z_image/__init__.py
@@ -1,1 +1,9 @@
 from .z_image import ZImageModel
+
+
+def __getattr__(name):
+    if name == "ZImageL2PModel":
+        from .z_image_l2p_model import ZImageL2PModel
+
+        return ZImageL2PModel
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
